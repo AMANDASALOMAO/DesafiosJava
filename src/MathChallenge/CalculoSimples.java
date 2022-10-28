1736154f56bc3120da6dd806bc9149dea1277736
AMANDASALOMAO <a.salo.andrade@outlook.com>
2022-10-28 17:14:56 -0300

    first commit

---
+++ src/MathChallenge/CalculoSimples.java
@@ -0,0 +1,56 @@
+/* O código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1. O código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago.
+
+Entrada
+O arquivo de entrada contém duas linhas de dados. Em cada linha haverá 3 valores, respectivamente dois inteiros e um valor com 2 casas decimais.
+
+Saída
+A saída deverá ser uma mensagem conforme o exemplo fornecido abaixo, lembrando de deixar um espaço após os dois pontos e um espaço após o "R$". O valor deverá ser apresentado com 2 casas após o ponto.
+
+Exemplo 1:
+
+Exemplos de Entrada	Exemplos de Saída
+12 1 5.30
+16 2 5.10
+
+VALOR A PAGAR: R$ 15.50
+
+Exemplo 2:
+
+Exemplos de Entrada	Exemplos de Saída
+13 2 15.30
+161 4 5.20
+
+VALOR A PAGAR: R$ 51.40
+
+Exemplo 3:
+
+Exemplos de Entrada	Exemplos de Saída
+1 1 15.10
+2 1 15.10
+
+VALOR A PAGAR: R$ 30.20
+
+*/
+
+package MathChallenge;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CalculoSimples {
+	
+  public static void main(String[] args) throws IOException {
+     try (Scanner leitor = new Scanner(System.in)) {
+
+		  int n1 = leitor.nextInt();
+		  double valor1 = leitor.nextDouble();
+		  
+		  int n2 = leitor.nextInt();
+		  double valor2 = leitor.nextDouble();
+			
+		 double total = n1*valor1 + n2*valor2;
+		 System.out.println(String.format("VALOR A PAGAR: R$ %.2f", total));
+	}   
+    }
+	
+}
